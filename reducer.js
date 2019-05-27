cc64b1c41b4d99133d0b4c39a1b3a8a72bcb5c2b
Anna Szmyga <anna.szmyga@o2.pl>
2019-05-27 16:52:05 +0200

    Add smaller reducer - comments

--- reducer.js
+++ reducer.js
@@ -1,44 +1,7 @@
-import {ADD_COMMENT, REMOVE_COMMENT, EDIT_COMMENT, THUMB_UP_COMMENT, THUMB_DOWN_COMMENT} from './actions'
+import { combineReducers } from 'redux';
+import comments from './comments';
 
-const initialState = {
-    comments: [],
-    users: []
-};
 
-function reducer(state = initialState, action) {
-    switch(action.type) {
-        case ADD_COMMENT:
-            return Object.assign({}, state, {
-                comments: [
-                {
-                    id: action.id,
-                    text: action.text,
-                    rating: 0
-                }
-                , ...state]
-            })
-        case REMOVE_COMMENT:
-            return Object.assign({}, state, {
-                comments: state.comments.filter(comment => comment.id !== action.id)
-            })
-        case EDIT_COMMENT:
-            return Object.assign({}, state, {
-                comments: state.comments.map(comment => {
-                    if (comment.id === action.id) {
-                        comment.text += 1;
-                    }
-                })
-            })
-        case THUMB_UP_COMMENT:
-        case THUMB_DOWN_COMMENT:
-            return Object.assign({}, state, {
-                comments: state.comments.map(comment => {
-                    if (comment.id === action.id) {
-                        comment.rating -= 1;
-                    }
-                })
-            })
-        default:
-            return state;
-    }
-}
+const app = combineReducers({
+    comments
+});
