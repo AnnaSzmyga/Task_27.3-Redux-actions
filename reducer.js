f384b90fbf96fba93416b4e6b55d536efdbf0b60
Anna Szmyga <anna.szmyga@o2.pl>
2019-05-27 15:42:08 +0200

    Change THUMB_UP_COMMENT and THUMB_DOWN_COMMENT actions

--- reducer.js
+++ reducer.js
@@ -7,38 +7,38 @@ const initialState = {
 
 function reducer(state = initialState, action) {
     switch(action.type) {
-    case ADD_COMMENT:
-        return Object.assign({}, state, {
-            comments: [
-            {
-                id: action.id,
-                text: action.text,
-                rating: 0
-            }
-            , ...state]
-        })
-    case REMOVE_COMMENT:
-        return Object.assign({}, state, {
-            comments: state.comments.filter(comment => comment.id !== action.id)
-        })
-    case EDIT_COMMENT:
-        return Object.assign({}, state, {
-            comments: state.comments.forEach(comment => {
-                if (comment.id === action.id) {
-                    comment.text = action.text;
+        case ADD_COMMENT:
+            return Object.assign({}, state, {
+                comments: [
+                {
+                    id: action.id,
+                    text: action.text,
+                    rating: 0
                 }
+                , ...state]
             })
-        })
-    case THUMB_UP_COMMENT:
-    case THUMB_DOWN_COMMENT:
-        return Object.assign({}, state, {
-            comments: state.comments.forEach(comment => {
-                if (comment.id === action.id) {
-                    comment.rating = action.rating;
-                }
+        case REMOVE_COMMENT:
+            return Object.assign({}, state, {
+                comments: state.comments.filter(comment => comment.id !== action.id)
             })
-        })
-    default:
-        return state;
+        case EDIT_COMMENT:
+            return Object.assign({}, state, {
+                comments: state.comments.map(comment => {
+                    if (comment.id === action.id) {
+                        comment.text += 1;
+                    }
+                })
+            })
+        case THUMB_UP_COMMENT:
+        case THUMB_DOWN_COMMENT:
+            return Object.assign({}, state, {
+                comments: state.comments.map(comment => {
+                    if (comment.id === action.id) {
+                        comment.rating -= 1;
+                    }
+                })
+            })
+        default:
+            return state;
     }
 }
